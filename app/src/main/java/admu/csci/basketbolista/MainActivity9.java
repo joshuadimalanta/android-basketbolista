67aff540695c7ba53e7d580be877b1e12d88132d
Virramon <vrking39@gmail.com>
2020-08-07 15:17:40 -0700

    updated MainActivity9 camera codes pero dein parin nagssave, kuha file path plng

--- app/src/main/java/admu/csci/basketbolista/MainActivity9.java
+++ app/src/main/java/admu/csci/basketbolista/MainActivity9.java
@@ -1,9 +1,17 @@
 package admu.csci.basketbolista;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,8 +22,17 @@ import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.os.Environment.getExternalStoragePublicDirectory;
+
 @EActivity(R.layout.activity_main9)
 public class MainActivity9 extends AppCompatActivity {
+
+    String pathToFile;
 
     // VARIABLES
     @ViewById(R.id.logoToHome3)
@@ -59,12 +76,47 @@ public class MainActivity9 extends AppCompatActivity {
         finish();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                Bitmap bitmap = BitmapFactory.decodeFile(pathToFile);
+//                save file path to realm db here TODO(1)
+            }
+        }
+    }
+
     @Click(R.id.buttonSelectPicture)
     public void selectPictureClick(View view){
         // code for camera
         // fix camera
-        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            photoFile = createPhotoFile();
+
+            if (photoFile != null) {
+                pathToFile = photoFile.getAbsolutePath();
+                Uri photoURI = FileProvider.getUriForFile(MainActivity9.this, "fssdfs", photoFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(intent, 1);
+            }
+
+        }
         startActivity(intent);
+    }
+
+    private File createPhotoFile() {
+        String name = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = null;
+        try {
+            image = File.createTempFile(name, ".jpg", storageDir);
+        } catch (IOException e) {
+            Log.d("mylog", "Excep : " + e.toString());
+        }
+        return image;
     }
 
     @Click(R.id.logoToHome3)
