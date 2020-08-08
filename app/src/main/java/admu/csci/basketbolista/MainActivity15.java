68ea9a97cd1b4553fe3d71cce826567bac3a56f0
joshuadimalanta <adrian.dimalanta@obf.ateneo.edu>
2020-08-08 19:22:09 +0800

    + camera, search, compare 2

---
+++ app/src/main/java/admu/csci/basketbolista/MainActivity15.java
@@ -0,0 +1,22 @@
+package admu.csci.basketbolista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main15)
+public class MainActivity15 extends AppCompatActivity {
+
+    // VARIABLES
+
+    @AfterViews
+    public void init(){
+
+    }
+
+}
